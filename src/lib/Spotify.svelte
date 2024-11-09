02cbbb90df738566aaaad79e1be50a81f2da360f
Remy Duivesteijn <remyd03@icloud.com>
2024-11-09 20:05:22 +0100

    v2.4 | Spotify widget static ⚡️

--- src/lib/Spotify.svelte
+++ src/lib/Spotify.svelte
@@ -1,5 +1,13 @@
 <div class="spotify">
-	<h2>Spotify</h2>
+	<div class="container-1">
+		<h2>Onlangs beluisterd</h2>
+		<strong>Money Trees</strong>
+		<span>Kendrick Lamar</span>
+	</div>
+	<div class="container-2">
+		<img class="img-1" src="/img/album.png" alt="" />
+		<img class="img-2" src="/img/record.png" alt="" />
+	</div>
 </div>
 
 <style>
@@ -8,13 +16,62 @@
 		border-radius: 1.25rem;
 		box-shadow: var(--box-shadow-hero);
 		width: 100%;
+		display: flex;
+		flex-direction: row;
+		position: relative;
+	}
+
+	.container-1 {
+		padding: 22px 0 22px 25px;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		gap: 5px;
 	}
 
 	h2 {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		color: #20d760;
-		height: 8.75rem;
+		font-size: 1rem;
+		font-weight: var(--font-weight-medium);
+		color: var(--dusty-lavender);
+	}
+
+	strong {
+		font-size: 1.25rem;
+		font-weight: var(--font-weight-semi-bold);
+		color: var(--slate-blue);
+		margin-top: auto;
+	}
+
+	span {
+		font-size: 14px;
+		color: var(--dusty-lavender);
+	}
+
+	.container-2 {
+		position: absolute;
+		top: 50%;
+		right: 2rem;
+		display: grid;
+		place-items: center;
+	}
+
+	.img-1 {
+		position: absolute;
+		width: 60px;
+		height: 60px;
+		z-index: 1;
+		border-radius: 50%;
+	}
+
+	.img-2 {
+		position: absolute;
+		width: 110px;
+		height: 110px;
+	}
+
+	@media (min-width: 930px) {
+		strong {
+			font-size: 1.5rem;
+		}
 	}
 </style>
