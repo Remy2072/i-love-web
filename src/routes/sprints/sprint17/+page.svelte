9a1299a6558c67c8a06a55315016670fb5ac9f51
Remy Duivesteijn <remyd03@icloud.com>
2024-11-25 22:09:25 +0100

    v5.1 | New headings ✨

--- src/routes/sprints/sprint17/+page.svelte
+++ src/routes/sprints/sprint17/+page.svelte
@@ -1,12 +1,109 @@
-<h1>
-	Het is nog geen 2 December 🤯
-</h1>
+<div class="container">
+    <div class="title">
+        <p class="emoji">💁🏼‍♀️</p>
+        <h1>User Needs</h1>
+        <small>Sprint 17</small>
+    </div>
+</div>
 
 <style>
+	* {
+		font-size: 1.125rem;
+		line-height: 1.5;
+	}
+
+	.container {
+		display: flex;
+		align-items: left;
+		flex-direction: column;
+		justify-content: center;
+		padding: 0 10%;
+	}
+
+	.emoji {
+		margin-top: 3.125rem;
+		font-size: 3.75rem;
+		text-shadow: var(--box-shadow-emoji);
+	}
+
+	.title {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
 	h1 {
-		display: grid;
-		height: 100vh;
-		width: 100%;
-		place-items: center;
+		font-size: 2.188rem;
+	}
+
+	small {
+		margin-top: 10px;
+		font-size: 1.25rem;
+		font-weight: 300;
+	}
+
+	h2 {
+		margin-top: 1.875rem;
+		font-size: 1.563rem;
+	}
+
+	.inhoud {
+		margin-top: 4.688rem;
+	}
+
+	h3 {
+		margin-top: 1.25rem;
+		font-size: 1.125rem;
+		color: var(--slate-blue);
+	}
+
+	img {
+		margin-top: 1.25rem;
+		border-radius: 15px;
+		border: 3px solid var(--dusty-lavender);
+	}
+
+	ul {
+		list-style-type: disc;
+		padding-left: 1.25rem;
+	}
+	ul li {
+		margin-bottom: 0.5em;
+	}
+	ul li ul {
+		margin-left: 1.25rem;
+	}
+
+	a {
+		color: var(--tropical-cyan);
+		text-decoration: underline;
+		margin: 0;
+	}
+
+	@media (min-width: 600px) {
+		.container {
+			padding: 0 20%;
+		}
+	}
+
+	@media (min-width: 900px) {
+		.container {
+			padding: 0 30%;
+		}
+
+		h1 {
+			font-size: 3.125rem;
+		}
+
+		h2 {
+			font-size: 1.563rem;
+		}
+	}
+
+	@media (min-width: 1300px) {
+		.container {
+			padding: 0 35%;
+		}
 	}
 </style>
