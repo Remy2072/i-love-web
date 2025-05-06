309e9df060490f0e5242ee22920d856151da7900
Remy <remy.duivesteijn@wearetriple.com>
2025-05-06 10:20:09 +0200

    v20.1 | bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script>
 	import { Nav, Page1, Page2, Page3 } from '$lib';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import { derived } from 'svelte/store';
 
-	let activePage = 'sprints';
+	const activePage = derived(page, ($page) => $page.url.searchParams.get('page'));
 
 	function handlePageChange(event) {
-		activePage = event.detail;
+		const newPage = event.detail;
+		const currentParams = new URLSearchParams($page.url.searchParams);
+		currentParams.set('page', newPage);
+		goto(`?${currentParams.toString()}`, { replaceState: true });
 	}
 </script>
 
@@ -13,11 +19,11 @@
 </header>
 
 <main>
-	{#if activePage === 'sprints'}
+	{#if $activePage === 'sprints'}
 		<Page1 />
-	{:else if activePage === 'we ♥ web'}
+	{:else if $activePage === 'we ♥ web'}
 		<Page2 />
-	{:else if activePage === 'portfolio'}
+	{:else if $activePage === 'portfolio'}
 		<Page3 />
 	{/if}
 </main>
